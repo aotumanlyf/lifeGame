0607cce389e4e39de092b05c2e974e7d3895fbfc
aotumanlyf <837667581@qq.com>
2020-04-13 18:29:03 +0800

    2020-4-13

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let box = document.getElementById('box')
 let row = []
 let dom = []
-let num = 4
+let num = 10
+
+// 表格
+// 横
 function createdRow(){
   for(let i = 0 ; i < num; i++){
     row[i] = document.createElement('div')
@@ -10,7 +13,7 @@ function createdRow(){
     createdCol(row[i], i)
   }
 }
-
+// 纵
 function createdCol(row, index){
   let col = []
   for(let i = 0; i < num; i++){
@@ -26,43 +29,58 @@ function createdCol(row, index){
   }
 }
 
+
+// 游戏规则
+
+// 开始
 function start(){
   setTimeout(() => {
     setInterval(() => {
       judge()
-    }, 1000);
+    }, 500);
   }, 5000);
 }
-
+// 判断状态
 function judge(){
-  // console.log(row)
-  let arr = []
-  console.log(dom)
+  let arr = JSON.parse(JSON.stringify(dom))
+  // console.log(dom)
   for(let i in dom){
-    // row[i].childNodes.foreach((item,index) => {
-    let leftTop = ly(dom[i - num - 1])
-    let top = ly(dom[i - num])
-    let rightTop = ly(dom[i - num + 1])
-    let left = ly(dom[i - 1])
-    let right = ly(dom[i + 1])
-    let leftBottom = ly(dom[i + num - 1])
-    let bottom = ly(dom[i + num])
-    let rightBottom = ly(dom[i + num + 1])
-    arr[i] = dom[i]
-    console.log(dom[1])
+    i = i * 1
+    let leftTop = survival(dom[i - num - 1])
+    let top = survival(dom[i - num])
+    let rightTop = survival(dom[i - num + 1])
+    let left = survival(dom[i - 1])
+    let right = survival(dom[i + 1])
+    let leftBottom = survival(dom[i + num - 1])
+    let bottom = survival(dom[i + num])
+    let rightBottom = survival(dom[i + num + 1])
+    // console.log(leftTop, top, rightTop, left, right, leftBottom, bottom, rightBottom)
+    // console.log(dom[i - 1], i - 1)
+    // console.log(dom[i + 1], i + 1)
+    // console.log(dom[1].className + '')
     // console.log(dom[i + 1])
     // console.log(leftTop + top + rightTop + left + right + leftBottom + bottom + rightBottom)
     if(leftTop + top + rightTop + left + right + leftBottom + bottom + rightBottom == 3){
-      arr[i].className = 'col alive'
+      arr[i].flag = 1
+    }else if(leftTop + top + rightTop + left + right + leftBottom + bottom + rightBottom == 2){
+      arr[i].flag = 2
     }else{
-      arr[i].className = 'col die'
+      arr[i].flag = 0
     }
   }
-  // console.log(arr)
-  dom = arr
+  for(let i in dom){
+    if( arr[i].flag == 1 ){
+      dom[i].className = 'col alive'
+    } else if( arr[i].flag == 2 ) {
+      // dom[i].className = 'col die'
+    } else {
+      dom[i].className = 'col die'
+    }
+  }
 }
 
-function ly(dom){
+// 是否存活
+function survival(dom){
   if(dom){
     return dom.className == 'col alive' ? 1 : 0
   }else{
@@ -70,6 +88,34 @@ function ly(dom){
   }
 }
 
+
+// 表单
+function createTable(){
+  let tableBox = document.createElement('div')
+  let table = document.createElement('div')
+  let input = document.createElement('input')
+  let startBtn = document.createElement('div')
+  tableBox.className = 'tableBox'
+  table.className = 'table'
+  input.className = 'input'
+  startBtn.className = 'startBtn'
+  startBtn.innerHTML = '进入游戏'
+  box.appendChild(tableBox)
+  tableBox.appendChild(table)
+  table.appendChild(input)
+  table.appendChild(startBtn)
+  startBtn.onclick = startClick()
+}
+
+function startClick(){
+  console.log(1)
+}
+
+// 创建表格
 createdRow()
+
+// 创建表单
+createTable()
+
 start()
 // judge()
